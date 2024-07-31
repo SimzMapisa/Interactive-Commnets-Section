3c38eb9c8d3bffd5bf74cb6fd3eb6c1eef9d4fec
SimzMapisa <simbarashe795@gmail.com>
2024-07-31 22:32:27 +0100

    Make changes to the comment creation lopgic
    Firstly created a content state management to get the input, the created
    a handle submit function with checks if the content is empty, if empty
    the function returns without submitting otherwise submits user input.

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,8 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
 const AddComment = (props) => {
+	const [content, setContent] = useState('');
+
 	const handleChange = (e) => {
 		setContent(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// Trim the content and check if it's empty
+		const trimmedContent = content.trim();
+
+		// if content is empty, clear the textre and return otherwise add the comment
+		if (trimmedContent === '') {
+			setContent('');
+			return;
+		}
+
+		const newComment = {
+			id: props.comments.length + 1,
+			user: props.currentUser,
+			content: content,
+			createdAt: new Date().toISOString(),
+			score: 0,
+			replies: [],
+		};
+
+		props.setComments([...props.comments, newComment]);
+		setContent('');
 	};
 
 	return (
@@ -15,11 +42,12 @@ const AddComment = (props) => {
 					className='border-2 border-light_gray w-full resize-none h-32 p-2 rounded-lg'
 					placeholder='Add a comment'
 					id=''
-					value=''
-					onChange={handleChange}
-				></textarea>
+					value={content}
+					onChange={handleChange}></textarea>
 			</form>
-			<button className='ml-4 bg-moderate_blue px-8 uppercase text-white font-medium py-2 rounded-lg'>
+			<button
+				className='ml-4 bg-moderate_blue px-8 uppercase text-white font-medium py-2 rounded-lg'
+				onClick={handleSubmit}>
 				send
 			</button>
 		</div>
