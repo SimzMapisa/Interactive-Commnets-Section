f8b4b3b6642acbe5e7db4b6523bca3e02ca7ecc8
SimzMapisa <simbarashe795@gmail.com>
2024-07-31 22:39:29 +0100

    Pass down the setComment method
    import useState to maintain state data state, comments destructured from
    data are stored in a new variable so that state can be updated when user
    submits comments. In addComment component setComments is passed as a
    prop this help to append a comment when submitted

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Comments from './components/Comments';
 import data from '../data.json';
 import AddComment from './components/AddComment';
+import { useState } from 'react';
 
 function App() {
 	const { comments, currentUser } = data;
+	const [allComments, setComments] = useState(data.comments);
 	return (
 		<main className='max-w-2xl mx-auto mt-4 relative'>
-			<Comments comments={comments} />
-			<AddComment currentUser={currentUser} comments={comments} />
+			<Comments comments={allComments} />
+			<AddComment
+				currentUser={currentUser}
+				comments={allComments}
+				setComments={setComments}
+			/>
 		</main>
 	);
 }
